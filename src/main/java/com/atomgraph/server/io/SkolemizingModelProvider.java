3ae3b3a75d40c031efd43942b4a1dcc2a96c5314
Martynas <martynas@graphity.org>
2017-10-25 16:25:32 +0200

    Add UUID dh:slug to documents before skolemization

--- src/main/java/com/atomgraph/server/io/SkolemizingModelProvider.java
+++ src/main/java/com/atomgraph/server/io/SkolemizingModelProvider.java
@@ -23,8 +23,17 @@ import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriBuilder;
 import com.atomgraph.server.exception.SkolemizationException;
 import com.atomgraph.processor.util.Skolemizer;
+import com.atomgraph.processor.vocabulary.DH;
+import java.util.Set;
+import java.util.UUID;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.vocabulary.RDF;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spinrdf.util.JenaUtil;
 
 /**
  *
@@ -40,10 +49,46 @@ public class SkolemizingModelProvider extends ValidatingModelProvider
     public Model process(Model model)
     {
         if (getRequest().getMethod().equalsIgnoreCase("POST"))
+        {
+            ResIterator it = model.listSubjects();
+            try
+            {
+                while (it.hasNext())
+                {
+                    Resource resource = it.next();
+                    process(resource); // add dh:slug to documents before skolemization
+                }
+            }
+            finally
+            {
+                it.close();
+            }
+        
             return skolemize(getOntology(), getUriInfo().getBaseUriBuilder(), getUriInfo().getAbsolutePathBuilder(),
                     super.process(model));
+        }
         
         return super.process(model);
+    }
+    
+    public Resource process(Resource resource)
+    {
+        // add UUID as dh:slug value to instances of dh:Container and dh:Item, if they don't have one
+        // unlike using STRUUID() in dh:DocumentConstructor, we can cache pages without worrying about reusing slugs
+        if (!resource.hasProperty(DH.slug))
+        {
+            Statement typeStmt = resource.getProperty(RDF.type);
+            if (typeStmt != null && typeStmt.getObject().isURIResource())
+            {
+                OntClass ontClass = getOntology().getOntModel().getOntClass(typeStmt.getResource().getURI());
+                // cannot use ontClass.hasSuperClass() here as it does not traverse the chain
+                Set<Resource> superClasses = JenaUtil.getAllSuperClasses(ontClass);
+                if (superClasses.contains(DH.Container) || superClasses.contains(DH.Item))
+                    resource.addLiteral(DH.slug, UUID.randomUUID().toString());
+            }
+        }
+
+        return resource;
     }
     
     public Model skolemize(Ontology ontology, UriBuilder baseUriBuilder, UriBuilder absolutePathBuilder, Model model)
