07ea2b0563c3b2cb7dff2362d2e367130b522311
Martynas <martynas@atomgraph.com>
2019-04-27 14:43:05 +0200

    Added SkolemizingModelProvider to application providers

--- src/main/java/com/atomgraph/server/Application.java
+++ src/main/java/com/atomgraph/server/Application.java
@@ -198,6 +198,7 @@ public class Application extends com.atomgraph.core.Application
         singletons.add(new TemplateProvider());
         singletons.add(new TemplateCallProvider());
         singletons.add(new SkolemizingDatasetProvider());
+        singletons.add(new SkolemizingModelProvider());
         singletons.add(new ResultSetProvider());
         singletons.add(new QueryParamProvider());
         singletons.add(new UpdateRequestReader());
