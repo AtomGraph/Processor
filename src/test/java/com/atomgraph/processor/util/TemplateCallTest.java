4f3e415750d8a2761d1f98ba315d4c1980ca338d
Martynas <martynas@atomgraph.com>
2019-05-26 10:30:00 +0200

    Extended TemplateCallTest

--- src/test/java/com/atomgraph/processor/util/TemplateCallTest.java
+++ src/test/java/com/atomgraph/processor/util/TemplateCallTest.java
@@ -27,14 +27,20 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 import org.apache.jena.enhanced.BuiltinPersonalities;
 import org.apache.jena.ontology.Ontology;
+import org.apache.jena.query.QuerySolutionMap;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.spinrdf.vocabulary.SP;
+import org.spinrdf.vocabulary.SPIN;
 import org.spinrdf.vocabulary.SPL;
 
 /**
@@ -44,12 +50,13 @@ import org.spinrdf.vocabulary.SPL;
 public class TemplateCallTest
 {
     
-    private static final String PREDICATE1_LOCAL_NAME = "a", PREDICATE2_LOCAL_NAME = "b", UNUSED_PREDICATE_LOCAL_NAME = "unused";
+    private static final String PREDICATE1_LOCAL_NAME = "a", PREDICATE2_LOCAL_NAME = "b", PREDICATE3_LOCAL_NAME = "c", UNUSED_PREDICATE_LOCAL_NAME = "unused";
     private static final String PARAM2_DEFAULT_VALUE = "default value";
+    private static final Literal QUERY_BINDING = ResourceFactory.createPlainLiteral("binding");
     
     private Template template;
-    private Property predicate1, predicate2, unusedPredicate;
-    private Parameter param1, param2;
+    private Property predicate1, predicate2, predicate3, unusedPredicate;
+    private Parameter param1, param2, param3;
     private Resource resource;
     private TemplateCall call;
     
@@ -64,12 +71,15 @@ public class TemplateCallTest
     public void setUp()
     {
         Ontology ontology = ModelFactory.createOntologyModel().createOntology("http://test/ontology");
-        
+        // ontology.addImport(LDT.NAMESPACE);
+
         predicate1 = ontology.getOntModel().createProperty("http://test/" + PREDICATE1_LOCAL_NAME);
         predicate2 = ontology.getOntModel().createProperty("http://test/" + PREDICATE2_LOCAL_NAME);
+        predicate3 = ontology.getOntModel().createProperty("http://test/" + PREDICATE3_LOCAL_NAME);
         unusedPredicate = ontology.getOntModel().createProperty("http://test/" + UNUSED_PREDICATE_LOCAL_NAME);
         
         param1 = ontology.getOntModel().createIndividual("http://test/ontology/param1", LDT.Parameter).
+                addProperty(RDF.type, SPL.Argument).
                 addProperty(SPL.predicate, predicate1). // following SPIN convention to use local name
                 addLiteral(SPL.optional, false).
                 addProperty(RDFS.isDefinedBy, ontology).
@@ -80,11 +90,27 @@ public class TemplateCallTest
                 addLiteral(SPL.defaultValue, PARAM2_DEFAULT_VALUE).
                 addProperty(RDFS.isDefinedBy, ontology).
                 as(Parameter.class);
+        param3 = ontology.getOntModel().createIndividual("http://test/ontology/param3", LDT.Parameter).
+                addProperty(SPL.predicate, predicate3). // following SPIN convention to use local name
+                addLiteral(SPL.optional, false).
+                addProperty(RDFS.isDefinedBy, ontology).
+                as(Parameter.class);
+
+        Resource spinQuery = ontology.getOntModel().createIndividual("http://test/ontology/spin/query", SP.Describe).
+                addLiteral(SP.text, "DESCRIBE * { ?a ?b ?c }").
+                addProperty(RDFS.isDefinedBy, ontology);
+        Resource spinTemplate = ontology.getOntModel().createIndividual("http://test/ontology/spin/template", SPIN.Template).
+                addProperty(SPIN.body, spinQuery).
+                addProperty(SPIN.constraint, param1). // ldt:Parameter rdfs:subClassOf spl:Argument
+                addProperty(RDFS.isDefinedBy, ontology);
 
         template = ontology.getOntModel().createIndividual("http://test/ontology/template", LDT.Template).
                 addLiteral(LDT.match, "{path}").
+                addProperty(LDT.query, ontology.getOntModel().createIndividual(spinTemplate).
+                    addLiteral(predicate1, QUERY_BINDING)).
                 addProperty(LDT.param, param1).
                 addProperty(LDT.param, param2).
+                addProperty(LDT.param, param3).
                 addProperty(RDFS.isDefinedBy, ontology).
                 as(Template.class);
         
@@ -134,7 +160,7 @@ public class TemplateCallTest
     }
     
     @Test(expected = ParameterException.class)
-    public void testValidateOptionals()
+    public void testValidateNonOptionals()
     {
         String param2Value = "with space";
         MultivaluedMap queryParams = new MultivaluedMapImpl();
@@ -144,16 +170,56 @@ public class TemplateCallTest
         call.applyArguments(queryParams).validateOptionals();
     }
     
+    public void testValidateOptionals()
+    {
+        String param1Value = "1", param2Value = "with space";
+        MultivaluedMap queryParams = new MultivaluedMapImpl();
+        queryParams.add(PREDICATE1_LOCAL_NAME, param1Value);
+        queryParams.add(PREDICATE2_LOCAL_NAME, param2Value);
+        // parameter3 is optional
+        
+        call.applyArguments(queryParams).validateOptionals();
+    }
+    
     @Test
     public void testValidateOptionalDefaults()
     {
-        String param1Value = "1";
+        String param1Value = "1", param3Value = "A";
         MultivaluedMap queryParams = new MultivaluedMapImpl();
         queryParams.add(PREDICATE1_LOCAL_NAME, param1Value);
-        // parameter2 is mandatory (spl:defaultValue false) and its value is missing, but it has a default vaulue which applies
-        
+        // parameter2 is mandatory (spl:defaultValue false) and its value is missing, but it has a default value which applies
+        queryParams.add(PREDICATE3_LOCAL_NAME, param3Value);
+
         TemplateCall applied = call.applyArguments(queryParams).applyDefaults().validateOptionals();
+        assertNotNull(applied.getArgument(predicate1));
         assertNotNull(applied.getArgument(predicate2));
+        assertNotNull(applied.getArgument(predicate3));
+    }
+
+    public void testArg()
+    {
+        String param1Value = "1", param2Value = "with space";
+        Resource param3Value = ResourceFactory.createResource("http://whateverest/");
+        MultivaluedMap queryParams = new MultivaluedMapImpl();
+        queryParams.add(PREDICATE1_LOCAL_NAME, param1Value);
+        queryParams.add(PREDICATE2_LOCAL_NAME, param2Value);
+        queryParams.add(UNUSED_PREDICATE_LOCAL_NAME, param3Value);
+
+        TemplateCall otherCall = TemplateCall.fromResource(resource, template).
+            arg(param1, ResourceFactory.createPlainLiteral(param1Value)).
+            arg(param2, ResourceFactory.createPlainLiteral(param2Value)).
+            arg(param3, param3Value);
+        
+        assertEquals(call.applyArguments(queryParams).build(), otherCall.build());
+    }
+
+    @Test
+    public void testGetQuerySolutionMap()
+    {
+        QuerySolutionMap qsm = new QuerySolutionMap();
+        qsm.add(predicate1.getLocalName(), QUERY_BINDING);
+        
+        assertEquals(qsm.asMap(), call.getQuerySolutionMap().asMap());
     }
     
 }
