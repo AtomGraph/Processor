78d321c752a79099d51f8b98d55a00a81606b090
Martynas <martynas@atomgraph.com>
2021-04-14 13:54:01 +0200

    Inject Optional<Ontology> into Item resource

--- src/main/java/com/atomgraph/server/resource/graph/Item.java
+++ src/main/java/com/atomgraph/server/resource/graph/Item.java
@@ -50,7 +50,7 @@ public class Item extends ResourceBase // TO-DO: extends GraphStore
     
     @Inject
     public Item(@Context UriInfo uriInfo, @Context Request request, @Context MediaTypes mediaTypes,
-            Service service, com.atomgraph.processor.model.Application application, Ontology ontology, Optional<TemplateCall> templateCall,
+            Service service, com.atomgraph.processor.model.Application application, Optional<Ontology> ontology, Optional<TemplateCall> templateCall,
             @Context HttpHeaders httpHeaders, @Context ResourceContext resourceContext)
     {
         super(uriInfo, request, mediaTypes,
